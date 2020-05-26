5f6fa9ae7a992807985e33215b001a2d1fac5777
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2020-05-26 17:02:01 -0400

    meet another component

--- src/App.js
+++ src/App.js
@@ -20,8 +20,7 @@ const list = [
   },
 ];
 
-function App() {
-  return (
+const App = () => (
     <div>
       <h1>My Hacker Stories</h1>
        
@@ -32,21 +31,18 @@ function App() {
       <List />  
     </div>
   );
-}
 
-function List() {
-  return list.map(function(item) {
-    return(
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.tile}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    )
-  });
-}
+
+  const List = () =>
+  list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 
 export default App;
