802c2387f1e2758ac6a304b12d36ce6ed0d2e648
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2020-05-27 11:42:03 -0400

    add prop exercise

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 3,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points : 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
-  //do something in between
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 3,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points : 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = event => {
     console.log(event.target.value);
   }
@@ -34,22 +33,22 @@ const App = () => {
       <input id="serach" type="text" onChange={handleChange}/>
 
       <hr />
-      <List />  
+      <List list={stories} />  
     </div>
   );
 };
 
 
-  const List = () =>
-  list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+  const List = props =>
+    props.list.map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    ));
 
 export default App;
