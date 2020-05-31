73090c001948b66aa4fe3128b1463f29c1537bf5
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2020-05-31 14:01:01 -0400

    React-Fragments

--- src/App.js
+++ src/App.js
@@ -59,11 +59,16 @@ const App = () => {
 };
 
 const Search = ({search, onSearch}) => (
-   <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" value={search} onChange={onSearch} />
-    </div>
-  );
+  <>
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
+  </>
+);
 
 //   return (
 //     <div>
