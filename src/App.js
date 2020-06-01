99dc7160eed886a301ef899968632af947d32bb9
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2020-06-01 15:12:57 -0400

    React-Conditional-Rendering

--- src/App.js
+++ src/App.js
@@ -44,11 +44,17 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React');
   
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() =>{
+    setIsLoading(true);
+
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    })
+    .catch(() => setIsError(true));
   }, []);
 
   const handleSearch = event => {
@@ -84,7 +90,16 @@ const App = () => {
       </InputWithLabel>
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />  
+      {isError && <p>Something went wrong ...</p>}
+        
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      )}
     </div>
   );
 };
